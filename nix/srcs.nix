9f2cbcf5088438b7adfd71678818d4725b00775c
Paweł Zaremba <pawzar@gmail.com>
2021-05-14 20:30:40 +0200

    Update oracle-suite package

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -1,52 +1,44 @@
 let
   inherit (builtins) map filter listToAttrs attrValues isString currentSystem;
-  inherit (import sources.nixpkgs {}) pkgs;
+  inherit (import sources.nixpkgs { }) pkgs;
   inherit (pkgs) fetchgit;
   inherit (pkgs.lib.strings) removePrefix;
 
   getName = x:
-   let
-     parse = drv: (builtins.parseDrvName drv).name;
-   in if isString x
-      then parse x
-      else x.pname or (parse x.name);
+    let parse = drv: (builtins.parseDrvName drv).name;
+    in if isString x then parse x else x.pname or (parse x.name);
 
   sources = import ./sources.nix;
   ssb-patches = ../ssb-server;
-in
 
-rec {
+in rec {
   inherit pkgs;
 
-  makerpkgs = import sources.makerpkgs {
-    dapptoolsOverrides.default = ./dapptools.nix;
-  };
+  makerpkgs = import sources.makerpkgs { dapptoolsOverrides.default = ./dapptools.nix; };
 
   nodepkgs = let
     nodepkgs' = import ./nodepkgs.nix { inherit pkgs; };
-    shortNames = listToAttrs (map
-      (x: { name = removePrefix "node_" (getName x.name); value = x; })
-      (attrValues nodepkgs')
-    );
+    shortNames = listToAttrs (map (x: {
+      name = removePrefix "node_" (getName x.name);
+      value = x;
+    }) (attrValues nodepkgs'));
   in nodepkgs' // shortNames;
 
   ssb-server = nodepkgs.ssb-server.override {
     name = "patched-ssb-server";
-    buildInputs = with pkgs; [ gnumake nodepkgs.node-gyp-build git ];
+    buildInputs = [ pkgs.gnumake nodepkgs.node-gyp-build pkgs.git ];
     postInstall = ''
       git apply ${ssb-patches}/ssb-db+19.2.0.patch
     '';
   };
 
-  setzer-mcd = makerpkgs.callPackage sources.setzer-mcd {};
+  setzer-mcd = makerpkgs.callPackage sources.setzer-mcd { };
 
-  stark-cli = makerpkgs.callPackage ../starkware {};
+  stark-cli = makerpkgs.callPackage ../starkware { };
 
-  oracle-suite = pkgs.callPackage sources.oracle-suite {};
+  oracle-suite = pkgs.callPackage sources.oracle-suite { };
 
   omnia = makerpkgs.callPackage ../omnia { inherit ssb-server setzer-mcd stark-cli oracle-suite; };
 
-  install-omnia = makerpkgs.callPackage ../systemd {
-    inherit omnia ssb-server oracle-suite;
-  };
+  install-omnia = makerpkgs.callPackage ../systemd { inherit omnia ssb-server oracle-suite; };
 }
